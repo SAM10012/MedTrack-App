01c8f716e35a1fb8d4fcb6d2cecf0111920059f7
Samadrita_Paul <paulsamadrita123@gmail.com>
2026-03-15 20:38:50 +0530

    Implemented GlobalExceptionHandler

---
+++ medtrack/src/main/java/com/pauls/medtrack/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.pauls.medtrack.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleAllExceptions(Exception ex, Model model) {
+
+        model.addAttribute("errorMessage", ex.getMessage());
+
+        return "error-page";
+    }
+
+}
